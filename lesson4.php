1b9e72119cd21ecbd9ce1deb9f85e636f74f59fc
Egi <endri.emini12@gmail.com>
2024-11-04 17:45:26 +0100

    asd
    asd

--- lesson4.php
+++ lesson4.php
@@ -73,26 +73,92 @@
 
 
 
-function callCounter() {
-    static $count = 0;
-    $count++;
-    echo "the value of count variable is : $count <br>";
+// function callCounter() {
+  //  static $count = 0;
+   // $count++;
+//    echo "the value of count variable is : $count <br>";
+//}
+//callCounter();
+//callCounter();
+
+
+
+//$sports = array('Football', 'Basketball', 'Handball', 'Voleyball');
+ //   echo count($sports);
+ //   array_push($sports, 'Golf');
+ //   var_dump($sports);
+
+
+
+//    $sports = array('Football','Basketball', 'Handball', 'Voleyball');
+//array_unshift($sports, 'Tennis');
+//var_dump($sports);
+
+
+
+
+$dogs = array(
+  array("Chihuahua", "Mexico", 25),
+  array("Husky", "Siberian",15),
+  array("Bulldog", "England",10)
+);
+
+echo $dogs[0][0]. " Origin ".$dogs[0][1]. ", Life Span ". $dogs[0][2]. "<br>";
+echo $dogs[1][0]. " Origin ".$dogs[1][1]. ", Life Span ". $dogs[1][2]. "<br>";
+echo $dogs[2][0]. " Origin ".$dogs[2][1]. ", Life Span ". $dogs[2][2]. "<br>";
+
+for($row = 0; $row<3; $row++){
+  echo "<p><b>Row Numbber $row</b></p>";
+  echo "<ul>";
+  for ($col = 0; $col<3; $col++){
+    echo "<li>".$dogs[$row][$col]."</li>";
+  }
+  echo "</ul>";
 }
-callCounter();
-callCounter();
+
+//challenge
+
+$array4D = [
+  [
+      [
+          ['Item 1-1-1-1', 'Item 1-1-1-2'],
+          ['Item 1-1-2-1', 'Item 1-1-2-2']
+      ],
+      [
+          ['Item 1-2-1-1', 'Item 1-2-1-2'],
+          ['Item 1-2-2-1', 'Item 1-2-2-2']
+      ]
+  ],
+];
+
+echo "<table border='1'>";
+
+for ($i = 0; $i < count($array4D); $i++) {
+  echo "<tr><td colspan='2'>Iphone 1: $i</td></tr>";
+  
+  for ($j = 0; $j < count($array4D[$i]); $j++) {
+      echo "<tr><td>Iphone 2: $j</td><td>";
+
+      for ($k = 0; $k < count($array4D[$i][$j]); $k++) {
+          echo "<div>Iphone 3: $k</div>";
+          echo "<ul>";
+
+          for ($l = 0; $l < count($array4D[$i][$j][$k]); $l++) {
+              echo "<li>Iphone 4: $l - " . $array4D[$i][$j][$k][$l] . "</li>";
+          }
+
+          echo "</ul>";
+      }
+      echo "</td></tr>";
+  }
+}
+
+echo "</table>";
 
 
 
-$sports = array('Football', 'Basketball', 'Handball', 'Voleyball');
-    echo count($sports);
-    array_push($sports, 'Golf');
-    var_dump($sports);
 
 
-
-    $sports = array('Football','Basketball', 'Handball', 'Voleyball');
-array_unshift($sports, 'Tennis');
-var_dump($sports);
 
 
 ?>
