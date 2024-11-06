e38bfa59d63f69067e40c392800809471f4b4b0d
Egi <endri.emini12@gmail.com>
2024-11-06 17:00:27 +0100

    asd
    asd

--- challenge.php
+++ challenge.php
@@ -1,52 +1,128 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        table{
-            width: 50%;
-            border-collapse: collapse;
-        }
-        tr,th, td{
-            padding: 10px;
-            text-align: left;
-            border: 1px solid black;
-        }
-        th{
-            background-color: #f2f2f2;
-            color: black;
-        }
-    </style>
-</head>
-<body>
-    <table border = "1">
-        <tr>
-            <th>Phones</th>
-            <th>In Stock</th>
-            <th>Sold</th>
-        </tr>
-
 <?php
- $phone = array(
-    array("Iphone 14", 10, 20),
-    array("Iphone 13", 10 , 30),
-    array("Iphone 12", 10 , 30),
-    array("Iphone 11", 10 , 30),
- );
+
+//$num = -1;
+
+//if($num<0){
+    //echo "$num is less than 0";
+//}
+
+//$age = 14;
+
+//if (($age > 12) && ($age < 15)){
+//    echo "Sale for you!!!";
+//}
+
+//if ($age > 18){
+ //   echo "you are an adult!!!";
+//}else {
+ //   echo " you are under 18!!!";
+//}
+
+//if($num<10){
+   // echo "the value of \$num is a niggative number!!!";
+//}elseif($num==0){
+    //echo "the value of \$num is equal to zero";
+//}else{
+//    echo "the value of \$num is a positive number";
+//}
 
 
- for($row=0; $row<4; $row++){
-    echo "<tr>";
-    for($col=0; $col<3; $col++){
-        echo "<td>".$phone[$row][$col]."</td>";
-    }
-    echo "</tr>";
- }
+
+// Function to determine the grade based on the score
+// function getGrade($score) {
+ //    if ($score >= 90 && $score <= 100) {
+ //        return 'A';
+ //    } elseif ($score >= 80 && $score <= 89) {
+  //       return 'B';
+  //   } elseif ($score >= 78 && $score <= 79) {
+  //       return 'C';
+  //   } elseif ($score >= 60 && $score <= 69) {
+  //       return 'D';
+  //   } else {
+  //       return 'F';
+  //   }
+// }
+
+// Get the student's score from user input
+// echo "Enter the student's score: ";
+// $handle = fopen("php://stdin", "r"); // Open the standard input stream
+// $score = trim(fgets($handle)); // Read input from the console
+// fclose($handle); // Close the stream
+
+// Validate the input to ensure it's a number
+// if (is_numeric($score) && $score >= 0 && $score <= 100) {
+    // Get the grade
+ //    $grade = getGrade($score);
+    // Output the grade
+    // echo "The student's grade is: $grade\n";
+// } else {
+ //    echo "Invalid score. Please enter a number between 0 and 100.\n";
+// }
 
 
- echo "</table>";
+// $age = 14; 
+
+// if ($age < 15) {
+ //    echo "You're too young.";
+// } else {
+ //    echo "You're old enough.";
+// }
+
+
+
+
+// $age = 16; 
+
+// if ($age >= 15 && $age < 18) {
+ //    echo "You're eligible.";
+// } else {
+ //    echo "You're not eligible.";
+// }
+
+
+
+// $age = 20; // Change this value to test different ages
+
+// if ($age > 18) {
+  //   echo "You're too old.";
+// } else {
+ //    echo "You're not too old.";
+// }
+
+
+
+//$arrays = array(
+  //array(1,2,3),
+ // array(1,2,3),
+ // array(1,2,3)
+//);
+
+//for($i = 0; $i < 4; $i++){
+ // for($j = 1; $j < 4; $j++){
+  //  echo "Array: $i Element: $j <br>";
+  //}
+//}
+
+
+//for($i=1; $i<5; $i++){
+ // for($j=1; $j<=$i; $j++){
+  //  echo "*";
+ // }
+  //echo "<br>";
+
+
+  $grade = array(
+    "math" => "3",
+    "art" => "5",
+    "history" => "4",
+    "music" => "4",
+  );
+  echo "math grade is ".$grade['math'];
+
+  //
+
+  foreach($grade as $subject => $grade){
+    echo "Subject: ".$subject . " / grade: ".$grade;
+    echo "<br>";
+  }
 ?>
-</body>
-</html>
