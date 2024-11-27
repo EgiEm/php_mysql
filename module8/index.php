62e6caf5122a3290da0ecbabef7d5bd4788caf91
EgiEm <164378350+EgiEm@users.noreply.github.com>
2024-11-27 17:49:12 +0100

    asd
    asd

--- module8/index.php
+++ module8/index.php
@@ -4,30 +4,39 @@
 //Creating a table using PDO
 
 
-$host = 'localhost';
-$db = 'db';
-$user = 'root';
-$pass = '';
+// $host = 'localhost';
+// $db = 'db';
+// $user = 'root';
+// $pass = '';
 
 
-try{
-    $pdo = new PDO("mysql:host= $host; dbname=$db", $user, $pass);
+// try{
+//     $pdo = new PDO("mysql:host= $host; dbname=$db", $user, $pass);
 
 
-    $sql = "CREATE TABLE users(id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(20) NOT NULL,
-    password VARCHAR(20) NOT NULL)";
+//     $sql = "CREATE TABLE users(id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+//     username VARCHAR(20) NOT NULL,
+//     password VARCHAR(20) NOT NULL)";
 
 
-    $pdo -> exec($sql);
+//     $pdo -> exec($sql);
 
 
-    echo "Table created successfully";
-}catch(exeption $e){
+//     echo "Table created successfully";
+// }catch(exeption $e){
 
 
-    echo "Error while creating the table".$e.getMessage();
+//     echo "Error while creating the table".$e.getMessage();
 
 
-}
+// }
+
+
+
+
+
+
+
+
+
 ?>
