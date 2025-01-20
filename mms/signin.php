40508a92e7493fbff9869f5a8a5b019fba59cf5e
EgiEm <164378350+EgiEm@users.noreply.github.com>
2025-01-20 17:43:57 +0100

    asd
    asd

--- mms/signin.php
+++ mms/signin.php
@@ -1,36 +1,3 @@
-<?php
-
-
-$error="";
-$success="";
-if($_SERVER["REQUEST_METHOD"]=="POST"){
-    $username=mysqli_real_escape_string($conn,$_POST['username']);
-    $email=mysqli_real_escape_string($conn,$_POST['email']);
-    $password=mysqli_real_escape_string($conn,$_POST['password']);
-
-
-    if(empty($username) || empty($email) || empty($password)){
-        $error='All fields are required';
-    }else{
-        $check_user="SELECT * FROM users WHERE username = '$username' OR email='$email'";
-        $result=$conn->query($check_user);
-
-
-        if($result->num_rows>0){
-            $error="Username or email already exists";
-        }else{
-            $hashed_password=md5($password);
-            $sql="INSERT INTO users(username,email,password) VALUES ('$username','$email','$hashed_password')"
-            if($conn->query($sql)===TRUE){
-                $success="Registration successful! You can now <a href='login.php'>Login Here </a>.";
-            }else{
-                $error="Error:" . $conn->error;
-            }
-        }
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -41,7 +8,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     <style>
         /* Background gradient for the entire page */
         body {
-            background: linear-gradient(135deg, #66785F, white);
+            background: linear-gradient(#155E95, #6A80B9, #FFF6B3 );
             color: #fff;
             font-family: 'Arial', sans-serif;
         }
